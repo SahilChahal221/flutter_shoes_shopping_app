9f0e03b549fe5c3135825de70fc4fa7f9bee8037
SahilChahal221 <145839855+SahilChahal221@users.noreply.github.com>
2023-10-20 22:20:32 +0530

    Add files via upload

---
+++ lib/global_variables.dart
@@ -0,0 +1,52 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Men Nike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Nike',
+    'sizes': [9, 10, 11, 12],
+  },
+  {
+    'id': '1',
+    'title': 'Addidas Shoes',
+    'price': 20.12,
+    'imageUrl': 'assets/images/shoes_2.png',
+    'company': 'Addidas',
+    'sizes': [9, 10, 12],
+  },
+  {
+    'id': '2',
+    'title': 'Bata Women Shoes',
+    'price': 28.95,
+    'imageUrl': 'assets/images/shoes_3.png',
+    'company': 'Bata',
+    'sizes': [7, 8, 9],
+  },
+  {
+    'id': '3',
+    'title': 'Jordan Shoes',
+    'price': 420.69,
+    'imageUrl': 'assets/images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+
+  {
+    'id': '4',
+    'title': 'Women Jordan Shoes',
+    'price': 399.69,
+    'imageUrl': 'assets/images/shoes_5.png',
+    'company': 'Nike',
+    'sizes': [6, 7, 8],
+  },
+
+  {
+    'id': '5',
+    'title': 'Bata Shoes',
+    'price': 39.69,
+    'imageUrl': 'assets/images/shoes_6.png',
+    'company': 'Nike',
+    'sizes': [8, 9, 10],
+  },
+];
